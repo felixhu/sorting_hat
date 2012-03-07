e455ea7d380bcdc9e8657006fc9e2bc8b0ee3782
Felix Hu <felixhu12@gmail.com>
2012-03-07 04:35:17 -0600

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SortingHat::Application.config.secret_token = 'd6d64e88a77a4d16eb40e0f539fe53e4c1734f35992c8c70bc2032d354e6479d09558c9e8ce4f576a78617983b38ff492b5c20edfdc1e2ca268532574c008bd8'
